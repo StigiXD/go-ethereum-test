34236757d1b6d3264760036b84e889099389c5fe
StigiXD <stagakisnikola@gmail.com>
2025-04-06 14:23:27 +0000

    Hardhat config settings update

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.28",
+  defaultNetwork: "test",
+  networks: {
+    hardhat: {
+    },
+    test: {
+        url: "http://127.0.0.1:8545"
+  	}
+  },
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
